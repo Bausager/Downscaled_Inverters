45e317ea0a52bd9d05d6cf1dcd014eba30464d70
Christian Lind Madsen <chrisser1234@live.dk>
2024-02-15 20:44:13 +0100

    just starting to make optimizations..

--- STM32F446/STM32CubeIDE/Core/Src/adc_handler.c
+++ STM32F446/STM32CubeIDE/Core/Src/adc_handler.c
@@ -7,167 +7,29 @@
 /* Includes ------------------------------------------------------------------*/
 #include "adc_handler.h"
 
+// Making this const because these settings doesnt change and therefore dont need to be in RAM but in flash. Hints const..
+const uint32_t ADC_channels[16] = {ADC_CHANNEL_0,ADC_CHANNEL_1,ADC_CHANNEL_2,ADC_CHANNEL_3,
+							 ADC_CHANNEL_4,ADC_CHANNEL_5, ADC_CHANNEL_6, ADC_CHANNEL_7,
+							 ADC_CHANNEL_8, ADC_CHANNEL_9, ADC_CHANNEL_10,ADC_CHANNEL_11,
+							 ADC_CHANNEL_12,ADC_CHANNEL_13,ADC_CHANNEL_14,ADC_CHANNEL_15};
 
-
-uint8_t ADC_Selector(uint8_t ADC_number, uint8_t Channal){
+uint8_t ADC_Selector(uint8_t ADC_number, uint8_t channel){
 	ADC_ChannelConfTypeDef sConfig = {0};
 	sConfig.Rank = 1;
 	sConfig.SamplingTime = ADC_SAMPLETIME_56CYCLES;
+	sConfig.Channel = ADC_channels[channel];
+
 	if(ADC_number == 1){
-		if(Channal == 0){
-			sConfig.Channel = ADC_CHANNEL_0;
-		}
-		else if(Channal == 1){
-			sConfig.Channel = ADC_CHANNEL_1;
-		}
-		else if(Channal == 2){
-			sConfig.Channel = ADC_CHANNEL_2;
-		}
-		else if(Channal == 3){
-			sConfig.Channel = ADC_CHANNEL_3;
-		}
-		else if(Channal == 4){
-			sConfig.Channel = ADC_CHANNEL_4;
-		}
-		else if(Channal == 5){
-			sConfig.Channel = ADC_CHANNEL_5;
-		}
-		else if(Channal == 6){
-			sConfig.Channel = ADC_CHANNEL_6;
-		}
-		else if(Channal == 7){
-			sConfig.Channel = ADC_CHANNEL_7;
-		}
-		else if(Channal == 8){
-			sConfig.Channel = ADC_CHANNEL_8;
-		}
-		else if(Channal == 9){
-			sConfig.Channel = ADC_CHANNEL_9;
-		}
-		else if(Channal == 10){
-			sConfig.Channel = ADC_CHANNEL_10;
-		}
-		else if(Channal == 11){
-			sConfig.Channel = ADC_CHANNEL_11;
-		}
-		else if(Channal == 12){
-			sConfig.Channel = ADC_CHANNEL_12;
-		}
-		else if(Channal == 13){
-			sConfig.Channel = ADC_CHANNEL_13;
-		}
-		else if(Channal == 14){
-			sConfig.Channel = ADC_CHANNEL_14;
-		}
-		else if(Channal == 15){
-			sConfig.Channel = ADC_CHANNEL_15;
-		}
 		if(HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK){
 			Error_Handler();
 		}
 	}
 	else if(ADC_number == 2){
-		if(Channal == 0){
-			sConfig.Channel = ADC_CHANNEL_0;
-		}
-		else if(Channal == 1){
-			sConfig.Channel = ADC_CHANNEL_1;
-		}
-		else if(Channal == 2){
-			sConfig.Channel = ADC_CHANNEL_2;
-		}
-		else if(Channal == 3){
-			sConfig.Channel = ADC_CHANNEL_3;
-		}
-		else if(Channal == 4){
-			sConfig.Channel = ADC_CHANNEL_4;
-		}
-		else if(Channal == 5){
-			sConfig.Channel = ADC_CHANNEL_5;
-		}
-		else if(Channal == 6){
-			sConfig.Channel = ADC_CHANNEL_6;
-		}
-		else if(Channal == 7){
-			sConfig.Channel = ADC_CHANNEL_7;
-		}
-		else if(Channal == 8){
-			sConfig.Channel = ADC_CHANNEL_8;
-		}
-		else if(Channal == 9){
-			sConfig.Channel = ADC_CHANNEL_9;
-		}
-		else if(Channal == 10){
-			sConfig.Channel = ADC_CHANNEL_10;
-		}
-		else if(Channal == 11){
-			sConfig.Channel = ADC_CHANNEL_11;
-		}
-		else if(Channal == 12){
-			sConfig.Channel = ADC_CHANNEL_12;
-		}
-		else if(Channal == 13){
-			sConfig.Channel = ADC_CHANNEL_13;
-		}
-		else if(Channal == 14){
-			sConfig.Channel = ADC_CHANNEL_14;
-		}
-		else if(Channal == 15){
-			sConfig.Channel = ADC_CHANNEL_15;
-		}
 		if(HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK){
 			Error_Handler();
 		}
 	}
 	else if(ADC_number == 3){
-		if(Channal == 0){
-			sConfig.Channel = ADC_CHANNEL_0;
-		}
-		else if(Channal == 1){
-			sConfig.Channel = ADC_CHANNEL_1;
-		}
-		else if(Channal == 2){
-			sConfig.Channel = ADC_CHANNEL_2;
-		}
-		else if(Channal == 3){
-			sConfig.Channel = ADC_CHANNEL_3;
-		}
-		else if(Channal == 4){
-			sConfig.Channel = ADC_CHANNEL_4;
-		}
-		else if(Channal == 5){
-			sConfig.Channel = ADC_CHANNEL_5;
-		}
-		else if(Channal == 6){
-			sConfig.Channel = ADC_CHANNEL_6;
-		}
-		else if(Channal == 7){
-			sConfig.Channel = ADC_CHANNEL_7;
-		}
-		else if(Channal == 8){
-			sConfig.Channel = ADC_CHANNEL_8;
-		}
-		else if(Channal == 9){
-			sConfig.Channel = ADC_CHANNEL_9;
-		}
-		else if(Channal == 10){
-			sConfig.Channel = ADC_CHANNEL_10;
-		}
-		else if(Channal == 11){
-			sConfig.Channel = ADC_CHANNEL_11;
-		}
-		else if(Channal == 12){
-			sConfig.Channel = ADC_CHANNEL_12;
-		}
-		else if(Channal == 13){
-			sConfig.Channel = ADC_CHANNEL_13;
-		}
-		else if(Channal == 14){
-			sConfig.Channel = ADC_CHANNEL_14;
-		}
-		else if(Channal == 15){
-			sConfig.Channel = ADC_CHANNEL_15;
-		}
 		if(HAL_ADC_ConfigChannel(&hadc3, &sConfig) != HAL_OK){
 			Error_Handler();
 		}
